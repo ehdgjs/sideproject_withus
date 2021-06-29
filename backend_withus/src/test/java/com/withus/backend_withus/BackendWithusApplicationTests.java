047d33eb335c8c64b22cfb71f2c7090549b1e4fd
ehdgjs <zone7907@gmail.com>
2021-06-29 21:38:09 +0900

    create project

---
+++ backend_withus/src/test/java/com/withus/backend_withus/BackendWithusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withus.backend_withus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWithusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
